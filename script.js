44083a071dd98e357b0acf1146d768a89916bde6
melodoc <l_tih@ro.ru>
2020-07-21 20:35:21 +0300

    Move success and fail into function

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ const isNumber = function(number) {
     return !isNaN(parseFloat(number)) && isFinite(number);
 };
 
-const randomInteger = function(min, max) {
-    const rand = Math.floor(min + Math.random() * (max + 1 - min));
-    return rand;
-};
-
 const guessRandomNumber = function() {
 
     const randomNumber = Math.floor(1 + Math.random() * 100);
@@ -14,10 +9,31 @@ const guessRandomNumber = function() {
 
     let attempts = 10;
 
-    const checkuserInput = function() {
+    function quizFailed() {
+        const repeatQuizFailed = confirm('Попытки закончились, хотите сыграть еще?');
+        if (repeatQuizFailed) {
+            const quiz = guessRandomNumber();
+            quiz();
+        } else {
+            alert('До свидания!');
+            return;
+        }
+    }
+
+    function quizSuccessed() {
+        const repeatQuizSucessed = confirm('Вы угадали число! Хотели бы сыграть еще?');
+        if (repeatQuizSucessed) {
+            attempts = 10;
+            const quiz = guessRandomNumber();
+            quiz();
+        } else {
+            alert('До свидания!');
+            return;
+        }
+    }
+
+    return function checkuserInput() {
         const userInput = prompt('Угадай чисто от 1 до 100', '100');
-        const quizFailed = 'Попытки закончились, хотите сыграть еще?';
-        const quizSucessed = 'Вы угадали число! Хотели бы сыграть еще?';
 
         if (userInput === null) {
             alert('До свидания!');
@@ -29,45 +45,23 @@ const guessRandomNumber = function() {
         } else if (userInput > randomNumber) {
             attempts--;
             if (attempts <= 0) {
-                const isRepeatQuizFailed = confirm(quizFailed);
-                if (isRepeatQuizFailed) {
-                    attempts = 10;
-                    const quiz = guessRandomNumber();
-                    quiz();
-
-                } else {
-                    alert('До свидания!');
-                    return;
-                }
-            } alert(`Загаданное число меньше. Осталось попыток: ${attempts}`);
+                quizFailed();
+                return;
+            }
+            alert(`Загаданное число меньше. Осталось попыток: ${attempts}`);
         } else if (userInput < randomNumber) {
             attempts--;
             if (attempts <= 0) {
-                const isRepeatQuizFailed = confirm(quizFailed);
-                if (isRepeatQuizFailed) {
-                    const quiz = guessRandomNumber();
-                    quiz();
-
-                } else {
-                    alert('До свидания!');
-                    return;
-                }
-            } alert(`Загаданное число больше.Осталось попыток: ${attempts}`);
-        } else if (userInput == randomNumber) {
-            const isRepeatQuizSucessed = confirm(quizSucessed);
-            if (isRepeatQuizSucessed) {
-                attempts = 10;
-                const quiz = guessRandomNumber();
-                quiz();
-            } else {
-                alert('До свидания!');
+                quizFailed();
                 return;
             }
+            alert(`Загаданное число больше. Осталось попыток: ${attempts}`);
+        } else if (userInput == randomNumber) {
+            quizSuccessed();
+            return;
         }
         checkuserInput();
     };
-
-    return checkuserInput;
 };
 
 const quiz = guessRandomNumber();
